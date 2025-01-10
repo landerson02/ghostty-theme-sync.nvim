d7323dbffc898fbc69aa3fe0f039becfe347aea2
Lucas Anderson <lucas.a@comcast.net>
2025-01-09 21:13:07 -0600

    Change config

--- lua/ghostty-theme-sync/config.lua
+++ lua/ghostty-theme-sync/config.lua
@@ -1,16 +1,12 @@
 local M = {}
 
 M.config = {
-	theme_path = "~/.ghostty-nvim-theme.txt",
+	-- Path to your ghostty config file
 	ghostty_config_path = "~/.config/ghostty/config",
 }
 
 function M.setup(user_config)
 	M.config = vim.tbl_extend("force", M.config, user_config or {})
-
-	if M.config.theme_path == nil then
-		error("You must define a theme file")
-	end
 
 	if M.config.ghostty_config_path == nil then
 		error("You must define a ghostty config path (make sure the file exists)")
