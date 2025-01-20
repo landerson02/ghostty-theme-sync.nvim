c2f4d703211ab42e234964cc84f90964d76fbb85
Lucas Anderson <lucas.a@comcast.net>
2025-01-20 15:03:18 -0600

    Add popular colorscheme support

--- lua/ghostty-theme-sync/translations.lua
+++ lua/ghostty-theme-sync/translations.lua
@@ -2,6 +2,9 @@ local translations = {
 	nvim_to_ghostty = {
 		["tokyonight-moon"] = "tokyonight_moon",
 		["tokyonight-night"] = "tokyonight_night",
+		["kanagawa-dragon"] = "Kanagawa Dragon",
+		["kanagawa-wave"] = "Kanagawa Wave",
+		["gruvbox"] = "GruvboxDark",
 	},
 }
 
