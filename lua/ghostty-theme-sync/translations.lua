19a6b78deda3679013de7e8cee8aa4ed43177ce9
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-29 15:59:50 +0100

    feat: add translations for 'projekt0n/github-nvim-theme'

--- lua/ghostty-theme-sync/translations.lua
+++ lua/ghostty-theme-sync/translations.lua
@@ -5,6 +5,19 @@ local translations = {
 		["kanagawa-dragon"] = "Kanagawa Dragon",
 		["kanagawa-wave"] = "Kanagawa Wave",
 		["gruvbox"] = "GruvboxDark",
+
+		-- "projekt0n/github-nvim-theme",
+		["github_dark"] = "GitHub Dark",
+		["github_dark_colorblind"] = "GitHub Dark",
+		["github_dark_default"] = "GitHub Dark",
+		["github_dark_dimmed"] = "GitHub Dark",
+		["github_dark_high_contrast"] = "GitHub Dark",
+		["github_dark_tritanopia"] = "GitHub Dark",
+		["github_light"] = "Github",
+		["github_light_colorblind"] = "Github",
+		["github_light_default"] = "Github",
+		["github_light_high_contrast"] = "Github",
+		["github_light_tritanopia"] = "Github",
 	},
 }
 
